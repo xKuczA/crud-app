bde0bf7d94fd137ce44156c218f82ee28b2468a2
xKuczA <115717692+xKuczA@users.noreply.github.com>
2024-12-09 23:39:35 +0100

    Update login.php

--- login.php
+++ login.php
@@ -1,4 +1,10 @@
 <?php
+session_start();
+if (isset($_SESSION['user'])) {
+    echo "Zalogowany użytkownik: " . $_SESSION['user'];
+}
+
+
 include 'header_guest.php';
 include 'db.php';
 session_start();
