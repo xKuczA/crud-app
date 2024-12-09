df9fc05c94de58745a6fa230c2fa4e1bf9af7b24
Paweł <pawelkuczera1@gmail.com>
2024-12-09 22:40:02 +0100

    Initial commit after resetting remote repository

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-require 'vendor/autoload.php';
+
 
 $client = new MongoDB\Client("mongodb+srv://pawelkuczera1:connect1@cluster0.jztpn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
 $db = $client->projekt_wypozyczalnia;
